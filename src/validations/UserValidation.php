f58bdac45c2b938d34e9268dc88232751b434f30
imajinyun <imajinyun@gmail.com>
2017-11-22 10:20:45 +0800

    : ) Update user validation

--- src/validations/UserValidation.php
+++ src/validations/UserValidation.php
@@ -2,14 +2,28 @@
 
 namespace App\Validation;
 
+use Phalcon\Validation;
+use Phalcon\Validation\Validator\Email;
+use Phalcon\Validation\Validator\PresenceOf;
+
 /**
  * User Validation.
  *
  * @package App\Validation
  */
-class UserValidation
+class UserValidation extends Validation
 {
     public function addValidate()
     {
+        $this->add('email', new PresenceOf(
+            [
+                'message' => '邮箱必须填写',
+            ]
+        ));
+        $this->add('email', new Email(
+            [
+                'message' => '邮箱地址有误',
+            ]
+        ));
     }
 }
