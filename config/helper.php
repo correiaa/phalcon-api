f495d080b1018ddabcda5ae47365a7fe003e6854
imajinyun <imajinyun@gmail.com>
2017-12-03 19:26:21 +0800

    : ) Update helper functions

--- config/helper.php
+++ config/helper.php
@@ -27,3 +27,68 @@ if ( ! function_exists('json')) {
         exit(1);
     }
 }
+
+if ( ! function_exists('logger')) {
+    /**
+     * Record application running log.
+     *
+     * @param string $msg
+     * @param int    $type
+     * @param string $name
+     * @param array  $context
+     *
+     * @return null
+     */
+    function logger(
+        $msg,
+        $type = \Phalcon\Logger::INFO,
+        $name = '',
+        array $context = null
+    ) {
+        $logger = function ($func = 'info', $name = '') use ($msg, $context) {
+            $y = date('Y');
+            $m = date('m');
+            $d = date('d');
+            $formatter = LOGS . '%s/%s/%s/%s.log';
+            $path = sprintf($formatter, $y, $m, $d, $name ?: $func);
+
+            if ( ! file_exists($path)) {
+                if (class_exists($class = \App\Component\Filesystem::class)) {
+                    $Filesystem = new \App\Component\Filesystem();
+                    $Filesystem->mkdir(dirname($path));
+                } else {
+                    throw new \RuntimeException("$class cannot be loaded.");
+                }
+            }
+            $Logger = new \Phalcon\Logger\Adapter\File($path);
+            $Logger->{$func}($msg, $context);
+        };
+
+        switch ($type) {
+            case 0:
+                $logger('emergence', $name);
+                break;
+            case 1:
+                $logger('critical', $name);
+                break;
+            case 2:
+                $logger('alert', $name);
+                break;
+            case 3:
+                $logger('error', $name);
+                break;
+            case 4:
+                $logger('warning', $name);
+                break;
+            case 5:
+                $logger('notice', $name);
+                break;
+            case 7:
+                $logger('debug', $name);
+                break;
+            default:
+                $logger('info', $name);
+                break;
+        }
+    }
+}
