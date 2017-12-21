6fc47bef3ac22da8e29edd3be69540ed56d42377
imajinyun <imajinyun@gmail.com>
2017-12-21 11:49:18 +0800

    : ) Update comment

--- src/Auth/AccountTypeInterface.php
+++ src/Auth/AccountTypeInterface.php
@@ -5,11 +5,7 @@ namespace App\Auth;
 /**
  * AccountType Interface.
  *
- * @copyright Copyright (c) Xiaohe Software Foundation, Inc.
- * @link      https://www.xiaohe.com/ Xiaohe(tm) Project
- * @package   App\Auth
- * @date      2017-12-20 21:21
- * @author    majinyun <majinyun@xiaohe.com>
+ * @package App\Auth
  */
 interface AccountTypeInterface
 {
