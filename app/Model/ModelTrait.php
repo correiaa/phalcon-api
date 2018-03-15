d0a9e3363c74721a1512dd1bc3095922e4c8612e
imajinyun <imajinyun@gmail.com>
2018-03-15 18:35:09 +0800

    : ) Update User model class.

--- app/Model/ModelTrait.php
+++ app/Model/ModelTrait.php
@@ -6,12 +6,32 @@ trait ModelTrait
 {
     public function beforeCreate()
     {
-        $this->createAt = date('Y-m-d H:i:s');
-        $this->updateAt = $this->createAt;
+        $datetime = $this->getDateTime();
+        $this->setCreatedAt($datetime);
+        $this->setUpdatedAt($datetime);
     }
 
     public function beforeUpdate()
     {
-        $this->updateAt = date('Y-m-d H:i:s');
+        $this->setUpdatedAt($this->getDateTime());
+    }
+
+    /**
+     * Get formatted datetime.
+     *
+     * @param string $format
+     * @param int    $timestamp
+     *
+     * @return string
+     */
+    public function getDateTime(
+        string $format = 'Y-m-d H:i:s',
+        int $timestamp = 0
+    ) : string {
+        if ($timestamp > 0) {
+            return date($format, $timestamp);
+        }
+
+        return date($format);
     }
 }
